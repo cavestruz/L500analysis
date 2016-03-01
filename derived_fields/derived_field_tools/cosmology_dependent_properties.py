a4dbe5ef72b8749a7704ffcb5d80682247e1f7e5
BabyOstrich <Camille Avestruz>
2016-03-01 15:58:49 -0600

    added cosmology_dependent_properties module in derived_field_tools package.  This includes a calculate_peak_height method, and a general get_cosmology method with default cosmology from the utils/constants module.

---
+++ derived_fields/derived_field_tools/cosmology_dependent_properties.py
@@ -0,0 +1,21 @@
+import L500analysis.utils.constants as constants
+from L500analysis.utils.utils import *
+import cosmolopy.perturbation as cp
+
+def calculate_peak_height(Mvir=None, redshift=None,aexp=None,
+                          cosmology=get_cosmology()) :
+
+    redshift = check_redshift_kwargs(redshift=redshift,aexp=aexp)
+
+    mass2radius = cp.mass_to_radius(Mvir/cosmology['h'], **cosmology)
+    
+    sigma_m = cp.sigma_r( mass2radius, redshift, **cosmology)[0]
+
+    return constants.delta_c / sigma_m
+    
+def get_cosmology(omega_M_0=constants.omega_m, omega_lambda_0=constants.omega_l,
+                  omega_b_0=constants.omega_b, omega_n_0=0.0, N_nu=0,
+                  h=constants.hubble, n=constants.power_spectrum_index_n,
+                  sigma_8=constants.sigma_8, baryonic_effects=True) :
+
+    return locals()
