88d5a56a5f686afb85b69b15c9bd5a9fc214801a
BabyOstrich <Camille Avestruz>
2016-03-08 12:57:57 -0600

    reorganized make_profile tools

---
+++ plotting/profiles/tools/make_profile.py
@@ -0,0 +1,45 @@
+'''Set up a profile to be plotted, interpolating for log-log plots,
+lin-log plots, etc.'''
+
+from numpy import *
+from L500analysis.utils.constants import rbins
+
+def make_profile(profile_rbins=rbins, x=None,y=None) :
+
+    return 10**interp(profile_rbins, x, log10(y))
+
+class MakeProfile :
+    def __init__(self, numbins=99, x=None, y=None, xlog=True, ylog=True) :
+        self.num_bins = numbins
+        self._create_volume_bins()
+
+        # Radial bins, equally spaced in log space for scaled profiles
+        # (for averaging)
+        self.rbins = 10**arange(log10(0.01),log10(8.0),0.05)
+        self.x = x
+        self.y = y
+        self._create_radial_data(xlog)
+        self._create_profile_data(ylog)
+
+    def _create_volume_bins(self) :
+        rr = 10**linspace(log10(10),log10(1e4),num=self.num_bins)
+        dlogr = log10(rr[1])-log10(rr[0])
+        self.rmid = zeros(num_bins)
+        self.dvol = zeros(num_bins)
+        self.rmid[0] = 5.0
+        self.dvol[0] =  4*pi/3.0*rr[0]**3.0
+        for i in xrange(1,self.num_bins) :
+            self.rmid[i] = 10**(1+dlogr*(i-0.5))
+            self.dvol[i] = 4*pi/3.0*(rr[i]**3.0-rr[i-1]**3.0)        
+
+    def _create_radial_data(self,xlog) :
+        self.x_interpolated = self.rbins
+
+    def _create_profile_data(self,ylog) :        
+        self.y_interpolated = 10**interp(self.rbins, self.x, 
+                                             log10(self.y))
+        
+
+# interp( rbins, rmid/h.rhalo,  log10(h.tpro*erg2keV/h.Thalo))
+
+# 10**interp (0.0, log10(rmid/h.rhalo), log10(h.tottpro*erg2keV/h.Thalo))
